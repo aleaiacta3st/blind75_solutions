155893fa1d5f3e3245d5ba9a97e2f71318f213ea
aleaiacta3st <invicinc@gmail.com>
2025-03-22 20:17:25 -0400

    Time: 0 ms (100%), Space: 19.3 MB (7.9%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -8,7 +8,56 @@ class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        queue=deque([root])
+        depth=0
+        while queue:
+            k=len(queue)#gives the total nodes at a level
+            for _ in range(k):
+                n=queue.popleft()
+                if (n.left):#add to queue only if the left child exists
+                    queue.append(n.left)
+                if (n.right):#add to queue only if the right child exists
+                    queue.append(n.right)
+            depth+=1
+        return depth
+
+# Count the number of levels
+# BFS processes nodes level-wise 
+# It means nodes in the left child and right child 
+# at the same level are processed together
+# The number of levels the BFS descends to is the total depth 
+
+# For this,use the length of the queue
+# increase depth only when all nodes at a level are processed
+# at the first level - we have only 1 node(root)
+# increase depth to 1 only after root has been popped
+
+# The for loop is designed in such a way that at the start of every
+# new while loop iteration the length of the queue contains exactly
+# the total number of nodes at that level 
+# So, now pop the queue exactly those many times to completely process
+# that level and add its children
+
+#         root
+#        /    \
+#      L1      R1
+#     /  \    /  \
+#   L2   R2  L3   R3
+#
+# Level 1: root
+# Level 2: L1, R1
+# Level 3: L2, R2, L3, R3
+#
+# BFS processes each level fully.
+# The count of levels processed = max depth of tree.
+
+
+
+
+#dfs starts here
+        # if not root:
+        #     return 0
+        # return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
 
 # def maxDepth(root):
