870c32a0047d50537066c3161dbfcb1d609f6b4e
aleaiacta3st <invicinc@gmail.com>
2025-03-30 22:00:31 -0400

    Time: 0 ms (100%), Space: 18.6 MB (79.73%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -7,12 +7,32 @@ class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if head is None:
             return None
-        prev = None #end of reversed list
+        prev = None 
         current = head 
         while current is not None:
-            temp = current.next 
-            current.next = prev
-            prev=current 
-            current = temp
+            temp = current.next #save the reference to the next node because 
+                                #we are about to remake connections
+            current.next = prev #make the current node point to its previous node
+            prev=current #Move prev forward to current
+            current = temp #Move current forward to temp
         return prev
-        
+
+     
+# Both temp and current.next are references (or pointers) to ListNode objects 
+# in memory, not the actual objects themselves.    
+
+# A ListNode object is a structure in memory that contains:
+# A value (val)
+# A reference to another node (next)
+
+# When we write temp = current.next:
+# We're not copying the node
+# We're just creating another reference to the same node
+# Both temp and current.next point to the identical object in memory
+
+# current and current.next point to different objects
+
+# Essentially, we're iterating through the list, flipping each node's next 
+# pointer to point to the previous node instead of the next node. 
+# The temp variable ensures we don't lose our way forward after breaking '
+# 'the original connections'
