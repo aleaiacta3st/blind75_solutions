ad71c34e361feb42907b57bd29dc30167eb5d036
aleaiacta3st <invicinc@gmail.com>
2025-04-02 00:50:49 -0400

    Time: 18 ms (20.33%), Space: 18.1 MB (33.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -18,4 +18,12 @@ class Solution:
                 if t_scan[s[i]]==0:
                     distinct_chars-=1
         return (distinct_chars==0)
+
+#easy when you have solved minimum window problem before
+# pitfall: in min window you check if t exists in s 
+# so it is possible that length s > length t 
+# but in this problem, lengths need to be equal 
+
+# after the initial check, we just see if all characters of t
+# are found in s with the same frequency.
         
