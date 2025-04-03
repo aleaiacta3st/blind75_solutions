7ea4c6be77276af34823b5df9b3b2f2b233079ae
aleaiacta3st <invicinc@gmail.com>
2025-04-03 05:23:07 -0400

    Time: 117 ms (86.49%), Space: 17.6 MB (97.76%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        n=len(s)
+        def expand_around_centre(x,y):
+            left=x 
+            right=y
+            count=0
+            while left>=0 and right<=n-1:
+                if s[left]==s[right]:#first check if initial positions are equal
+                    count+=1
+                    left=left-1
+                    right=right+1
+                else:
+                    return count #when chars do not match return count seen so far
+            return count #still returns count should the while loop fail
+        
+        count=0
+        for i in range(n):
+            count_odd = expand_around_centre(i,i)
+            count_even = expand_around_centre(i,i+1)
+            count=count+count_odd+count_even
+        return count
+        
