2519f1075d2d5554fcbbe0812695bd90c8ad4f50
aleaiacta3st <invicinc@gmail.com>
2025-08-27 23:37:33 -0400

    Time: 13 ms (97.96%), Space: 23.4 MB (40.96%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,43 +1,14 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n=len(nums) 
-        #will create a list with 1s as its elements
-        result = [1] * n 
-
-        #the first element has no product to its left 
-        # hence we must set its left proudct to 1, so that 
-        # its entire product evaluates to its right product
-
-        # we need left product x right product
-
-        left_product=1 
-        #we now move from left to right till the element in focus
+        result = []
+        n=len(nums)
+        result = [1]*n
+        left_product=1
         for i in range(n):
             result[i] = left_product
-            #observe that the above line, in the first iteration 
-            # sets result[0]=1 which is what we need. The next 
-            # element will now use left_product=first element 
-            left_product *= nums[i]
-
-        
-        #we now move right to left till the element in focus
-        #range syntax is start,stop,step
-        #if step<0, it means we need to go back, decreases
-        #so we start from greater i to smaller i, 
-        # this happens only when we move from right to left
+            left_product=left_product*nums[i]
         right_product=1
         for i in range(n-1,-1,-1):
-        #element with index(n-1) is the last element
-        #stop -1, so that will not be included, 
-        #    final value i takes would be=0
-        #-1 step, the values that i takes are decreasing
-            result[i] *= right_product 
-            #observe the change above when compared to the left product.
-            #result[i] already has the left product in it before 
-            # multiplicatopn by the right_product
-            right_product *= nums[i]
-
-        return result 
-
-
-        
+            result[i] = result[i]*right_product
+            right_product = right_product*nums[i]
+        return result
