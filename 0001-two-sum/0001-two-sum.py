54b479e2ea9cd9026cc31ea84278589a965ff654
aleaiacta3st <invicinc@gmail.com>
2025-11-23 23:44:41 -0500

    Time: 0 ms (100%), Space: 19 MB (34.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,20 @@
 class Solution:
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     container = {}  # Key: number, Value: index
+    #     for i in range(len(nums)):
+    #         complement = target - nums[i]  # What we need to find
+    #         if complement in container:   # Already seen it?
+    #             return [container[complement], i]  # Return indices
+    #         container[nums[i]] = i  # Add current number to map
+    #     return []  # No solution found   
+
+
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        container = {}  # Key: number, Value: index
-        for i in range(len(nums)):
-            complement = target - nums[i]  # What we need to find
-            if complement in container:   # Already seen it?
-                return [container[complement], i]  # Return indices
-            container[nums[i]] = i  # Add current number to map
-        return []  # No solution found   
-        
+        container = {}
+        n = len(nums)
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in container:
+                return(container[complement],i )
+            container[nums[i]] = i
