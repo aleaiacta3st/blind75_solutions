45ae9f82013ac18e896e5dca710ba862e88ac5d3
aleaiacta3st <invicinc@gmail.com>
2025-11-25 00:01:56 -0500

    Time: 19 ms (87.32%), Space: 23.4 MB (55.35%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,14 +1,34 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        result = []
-        n=len(nums)
-        result = [1]*n
-        left_product=1
+        n=len(nums) 
+        result = [1]*n 
+
+        # left_product is the product accumulator
+
+        # observe that
+
+        # left_product(current number) = left_product(previous number) * previous number
+
+
+        #for the first element, its left product should be 1
+        left_product = 1
+
         for i in range(n):
+
             result[i] = left_product
-            left_product=left_product*nums[i]
-        right_product=1
+            #update left_product for the next i
+            left_product = left_product*nums[i]
+
+
+
+
+        right_product=1    
+
         for i in range(n-1,-1,-1):
             result[i] = result[i]*right_product
-            right_product = right_product*nums[i]
+            right_product=right_product*nums[i]
+
         return result
+
+
+        
