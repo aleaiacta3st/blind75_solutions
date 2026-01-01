f1c456973e625024413fa8edb712fca702e3dcce
aleaiacta3st <invicinc@gmail.com>
2025-12-31 22:23:34 -0500

    Time: 4 ms (19.94%), Space: 19.1 MB (89.33%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,18 +1,63 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        updated_intervals=[]
-        for start,end in intervals:
-            if end<newInterval[0]:
-                updated_intervals.append([start,end])
-            if end>=newInterval[0] and start<=newInterval[1]:
-                start_merge = min(start,newInterval[0])
-                end_merge = max(end,newInterval[1])
-                newInterval=[start_merge,end_merge]
-        updated_intervals.append(newInterval)
-        for start,end in intervals:
-            if start>newInterval[1]:
-                updated_intervals.append([start,end])
-        return updated_intervals
+        result = []
+        i = 0
+        n = len(intervals)
+        
+        # 1. Add intervals ending before newInterval starts
+        while i < n and intervals[i][1] < newInterval[0]:
+            result.append(intervals[i])
+            i += 1
+        
+        # 2. Merge overlapping intervals
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        result.append(newInterval)
+        
+        # 3. Add remaining intervals
+        while i < n:
+            result.append(intervals[i])
+            i += 1
+        
+        return result
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # updated_intervals=[]
+        # for start,end in intervals:
+        #     if end<newInterval[0]:
+        #         updated_intervals.append([start,end])
+        #     if end>=newInterval[0] and start<=newInterval[1]:
+        #         start_merge = min(start,newInterval[0])
+        #         end_merge = max(end,newInterval[1])
+        #         newInterval=[start_merge,end_merge]
+        # updated_intervals.append(newInterval)
+        # for start,end in intervals:
+        #     if start>newInterval[1]:
+        #         updated_intervals.append([start,end])
+        # return updated_intervals
 
 
 # The key insight is to keep updating the new interval
