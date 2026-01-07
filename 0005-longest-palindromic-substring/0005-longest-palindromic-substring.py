a40c0f3b410a68ae0c29a8a19d8e9002f1a53daa
aleaiacta3st <invicinc@gmail.com>
2026-01-07 00:37:26 -0500

    Time: 243 ms (89.92%), Space: 19.5 MB (15.86%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,35 +1,20 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n=len(s)
-        def expand_around_centre(x,y):
-            left=x 
-            right=y
-            max_size=0 
-            while left>=0 and right<=n-1:
-                if s[left]==s[right]:#first check if initial positions are equal
-                    current_size=right-left+1
-                    if current_size>max_size:
-                        max_size=current_size
-                    left=left-1
-                    right=right+1
-                else:
-                    return (max_size,left+1) #when chars do not match return the max size seen so far
-            return (max_size,left+1) #still returns the max size should the while loop fail
-        
-        greatest_size=0
-        start=0
-        for i in range(n):
-            odd_size,odd_start = expand_around_centre(i,i)
-            even_size,even_start = expand_around_centre(i,i+1)
-            if odd_size> even_size:
-                if odd_size>greatest_size:
-                    greatest_size=odd_size
-                    start=odd_start
-            else: 
-                if even_size>greatest_size:
-                    greatest_size=even_size
-                    start=even_start
-        return s[start:start+greatest_size]
+        def expand(l, r):
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return l + 1, r  # bounds after over-expansion
+    
+        start, end = 0, 0
+        for i in range(len(s)):
+            l1, r1 = expand(i, i)      # odd
+            l2, r2 = expand(i, i + 1)  # even
+            if r1 - l1 > end - start:
+                start, end = l1, r1
+            if r2 - l2 > end - start:
+                start, end = l2, r2
+        return s[start:end]
 
 # def expand_around_centre(x,y)
 # a helper function which takes two arguments, the indices where left and right initially start and returns the max size of palindrome around that center 
