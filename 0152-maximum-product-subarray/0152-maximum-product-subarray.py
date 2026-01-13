49667a3e5079207f06f0a9e0c328708b0e4d3df2
aleaiacta3st <invicinc@gmail.com>
2026-01-12 22:18:30 -0500

    Time: 7 ms (85.44%), Space: 19.9 MB (11.65%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,30 +1,14 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        max_prod_so_far = nums[0]
-        min_prod_so_far = nums[0]
-        max_global=nums[0]
+        max_i=nums[0]
+        min_i=nums[0]
+        max_product=nums[0]
+        n=len(nums)
 
-        #in max sum subarray, we discarded previous negative sum
-        #but in product we cannot do that
-        #as a negative product can multiply with a negative number in the future 
-        #and become positive
-        #what is min_prod_so_far tracking?
-        # consider all subarrays ending at element i
-        #someof them have negative products, some of them have positive products
-        #that is what we are tracking
-        #the ith element can start a new subarray if it does not combine with
-        #the previous sequence or it can combine with the positive product
-        #subarray or the negative product subarray
-    
-        for num in nums[1:]:
-            k=max_prod_so_far
-            max_prod_so_far = max(num, num*max_prod_so_far, num*min_prod_so_far)
-            #max_prod_so_far is updated above. so store its value in k so
-            #it can be used below
-            min_prod_so_far = min(num, num*k, num*min_prod_so_far)
+        for i in range(1,n):
+            candidates = (max_i*nums[i], min_i*nums[i], nums[i])
+            max_i = max(candidates)
+            min_i = min(candidates)
+            max_product=max(max_product,max_i)
 
-            max_global = max(max_global, max_prod_so_far)
-            
-
-        return max_global
-        
+        return max_product
