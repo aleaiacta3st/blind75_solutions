0ebaca35bfbfd26d186fa9d9ec57a46cdfaab415
aleaiacta3st <invicinc@gmail.com>
2026-01-14 01:22:27 -0500

    Time: 0 ms (100%), Space: 19.5 MB (7.33%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -1,25 +1,20 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        x, y = abs(a), abs(b)
-        # ensure x >= y
-        # this way if they ahve opposite signs, 
-        # adding them won't land us in negative territory
+        x = abs(a)
+        y = abs(b)
+
         if x < y:
-            return self.getSum(b, a)  
-        #recursion happens only once
+            return self.getSum(b, a)
+
         sign = 1 if a > 0 else -1
-        #we have ensured a to be the absolute biggest number. 
-        # so sign depends on a and not on b.
-        
-        if a * b >= 0:#same sign, so just add the numbers. 
-                    #sign will be added at the end.
-            # sum of two positive integers
+
+        if a * b >= 0:
             while y:
                 x, y = x ^ y, (x & y) << 1
-        else: #we need to subtract
-            # difference of two positive integers
+        else:
             while y:
                 x, y = x ^ y, ((~x) & y) << 1
-        
+
         return x * sign
+        
         
