746936d663be7508dad08d3673d931a52d12652e
aleaiacta3st <invicinc@gmail.com>
2026-01-14 01:56:32 -0500

    Time: 49 ms (18.23%), Space: 20.3 MB (10.41%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,13 +1,14 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        dp=[0]*(n+1)
-        #write numbers from 1 to 9 in binary
-        #observe that even numbers have the same number of 1s as their half
-        #odd numbers have = number of 1s in their half + 1
-        for i in range(1,n+1):
-            if (i%2 == 1):
-                dp[i] = dp[i//2]+1 #instead of i//2, i>>1 can also be used
-            else:
-                dp[i] = dp[i//2]    
-        return dp
+        ans=[]
+
+        for i in range(n+1):
+            count=0
+            while i:
+                count=count+(i&1)
+                i=(i>>1)
+            ans.append(count)
+
+        return ans
+
         
