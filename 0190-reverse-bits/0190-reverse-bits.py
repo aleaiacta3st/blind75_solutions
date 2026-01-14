396e0c8668e722963f94dd10ca03b4e2b8499ef3
aleaiacta3st <invicinc@gmail.com>
2026-01-14 03:37:17 -0500

    Time: 47 ms (13.39%), Space: 19.5 MB (8.69%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,16 +1,9 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        number=0
-        for i in range(32):#because 3 in 32 bit will be 
-                        #preceded by many zeros
-            number = number<<1 #shift the number to make place 
-                            #for the last bit that will be 
-                            # extracted in the next step
-            if (n&1==1):#last bit is 1
-                number = number+1 #number|1 will give the same result
-                #if last bit is 0, number =number+0, no change in number
-                #so there is no need of a separate else condition for it
-            n=n>>1#push out the last bit,
-                #we have already taken care of it
-        return number
+        result =0
+        for _ in range(32):
+            result = result<<1 | n&1
+            n=n>>1
+        return result
+        
         
