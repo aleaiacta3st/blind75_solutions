bce6d988e11a097598886f5e7b856a5946f14373
aleaiacta3st <invicinc@gmail.com>
2026-01-14 19:21:08 -0500

    Time: 3 ms (4.75%), Space: 19.4 MB (11.18%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -2,15 +2,14 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         n=len(nums)
         left=0
-        right = n-1
+        right=n-1
 
         while left<=right:
             mid = (left+right)//2
-            
-            if nums[mid]==target:
-                return mid 
 
-            if nums[left]<=nums[mid]:
+            if target==nums[mid]:
+                return mid
+            elif nums[left]<=nums[mid]:
                 if nums[left]<=target<nums[mid]:
                     right=mid-1
                 else:
@@ -20,7 +19,7 @@ class Solution:
                     left=mid+1
                 else:
                     right=mid-1
-
         return -1
+
 
         
