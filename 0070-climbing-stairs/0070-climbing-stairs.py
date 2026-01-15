427dc1e335623ef05f2443cb16dc424d81b04c08
aleaiacta3st <invicinc@gmail.com>
2026-01-14 20:32:05 -0500

    Time: 0 ms (100%), Space: 19.3 MB (8.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -5,16 +5,14 @@ class Solution:
             return 1
         if n==1:
             return 1
-        if n==2:
-            return 2
 
         dp=[0]*n
 
         dp[0]=1
         dp[1]=2
-        dp[2]=3
+        
 
-        for i in range(3,n):
+        for i in range(2,n):
             dp[i]=dp[i-1]+dp[i-2]
 
         return dp[n-1]
