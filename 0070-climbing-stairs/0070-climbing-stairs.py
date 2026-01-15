7ff73de7932e6c17392887a65c55e29a50776d1e
aleaiacta3st <invicinc@gmail.com>
2026-01-14 23:12:29 -0500

    Time: 0 ms (100%), Space: 19.4 MB (8.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,15 +3,45 @@ class Solution:
         if n==1:
             return 1
 
-        prev2=1
-        prev1=1
+        dp=[0]*(n+1)
+
+        dp[0]=1
+        dp[1]=1
 
         for i in range(2,n+1):
-            curr=prev2+prev1
-            prev2=prev1
-            prev1=curr
+            dp[i]=dp[i-1]+dp[i-2]
 
-        return prev1
+        return dp[n]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # if n==1:
+        #     return 1
+
+        # prev2=1
+        # prev1=1
+
+        # for i in range(2,n+1):
+        #     curr=prev2+prev1
+        #     prev2=prev1
+        #     prev1=curr
+
+        # return prev1
 
 
         
