30d3289415f0752717299ea03d9ded0bb1163511
aleaiacta3st <invicinc@gmail.com>
2026-01-22 17:37:59 -0500

    Time: 4 ms (71.26%), Space: 21.4 MB (5.47%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,49 +1,32 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        adj_list = [[] for _ in range(n)]
-        for edge in edges:
-            u,v=edge 
-            adj_list[u].append(v)
-            adj_list[v].append(u)
-        adj_list_dict={}
-        for k in range(n):
-            adj_list_dict[k] = adj_list[k]
-
         visited=set()
 
-        def dfs(i):
-            if i not in visited:
-                visited.add(i)
-                for neighbor in adj_list[i]:
-                    if neighbor not in visited:
-                        dfs(neighbor)
+        adj_dict={}
 
-        no_of_connected_components=0
 
         for i in range(n):
-            if i not in visited:
-                dfs(i)
-                no_of_connected_components+=1
+            adj_dict[i] = []
 
-        return no_of_connected_components
 
-# every time you encounter a node not yet visited, 
-# that’s the start of a new connected component.
+        for node,neighbor in edges:
+            adj_dict[node].append(neighbor)
+            adj_dict[neighbor].append(node)
 
-#create an adjacency list from the given edges array
-#start dfs
-#A dfs at a node will traverse all the nodes connected to it
-#to prevent getting stuck due to cycles, use a visited set
 
-# Visual Example of Multiple Components:
-#
-#    0 — 1 — 2        (Component 1)
-#
-#    3 — 4            (Component 2)
-#
-#    5                (Component 3, isolated node)
-#
-# Start DFS from 0: visits (0,1,2) — component count = 1
-# Next unvisited: node 3 — visits (3,4) — component count = 2
-# Next unvisited: node 5 — visits (5) — component count = 3
+        def dfs(node):
+            if node in visited:
+                return
+            visited.add(node)
+            for neighbor_nodes in adj_dict[node]:
+                dfs(neighbor_nodes)
+
+        count=0
+
+        for node in adj_dict:
+            if node not in visited:
+                count=count+1
+                dfs(node)
+
+        return count
         
