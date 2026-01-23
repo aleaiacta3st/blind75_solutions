bbc97aee0580cfa5edc91293ba962c6288d665a1
aleaiacta3st <invicinc@gmail.com>
2026-01-23 08:39:57 -0500

    Time: 7 ms (82.85%), Space: 19.6 MB (36.58%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,30 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        if s=="":
-            return True 
-        n=len(s)
-        s=s.lower()
-        only_alphanumeric=""
-        for i in s:
-            if i in ['0','1','2','3','4','5','6','7','8','9']:
-                only_alphanumeric+=i
-            if i in string.ascii_lowercase:
-                only_alphanumeric+=i
-        original=[] #this is a stack
-        for i in only_alphanumeric:
-            original.append(i)
-        popped_out = ''
-        for i in range(len(only_alphanumeric)):
-            popped_out +=original.pop() 
-        return only_alphanumeric==popped_out
+        l=0
+        r=len(s)-1
 
-# s = "hello"
-# k = s       # k and s both point to "hello"
-# s = s.upper()  # now s points to "HELLO", k still points to "hello"
 
-# convert to lower case 
-# remove any non alphanumeric chars 
-# stack 
-# pop 
-# if both are equal. it is a palindrome.
+        while l<r:
+            while l<r and not s[l].isalnum():
+                l+=1
+            while l<r and not s[r].isalnum():
+                r-=1
+            if s[l].lower()!=s[r].lower():
+                return False
+            l+=1
+            r-=1
+
+        return True
+
         
