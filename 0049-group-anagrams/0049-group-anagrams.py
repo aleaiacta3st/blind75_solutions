d28ba5c8800fb1a61ce5d36d0ea5528fed69aa9e
aleaiacta3st <invicinc@gmail.com>
2026-01-23 08:06:21 -0500

    Time: 12 ms (59.9%), Space: 22.2 MB (35.41%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,51 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        grouped_anagrams={}
-        n=len(strs)
-        for i in range(n):
-            if ''.join(sorted(strs[i])) in grouped_anagrams:
-                grouped_anagrams[''.join(sorted(strs[i]))].append(strs[i])
-            else:
-                grouped_anagrams[''.join(sorted(strs[i]))] = [strs[i]]
-        return list(grouped_anagrams.values())
+        groups={}
 
-# sorted(strs[i]):
-# Takes a string (like "eat") and converts it to a sorted list of characters
-# Example: "eat" → ['a', 'e', 't']
-# This gives us our characters in alphabetical order
+        for s in strs:
+            key=''.join(sorted(s))
+            if key not in groups:
+                groups[key]=[]
+            groups[key].append(s)
 
-# ''.join(...):
-# The empty string '' is the "glue" between elements
-# The join() method combines all characters in the list into a single string
-# Example: ['a', 'e', 't'] → "aet"
-
-# Strings in Python are immutable, which means once created, they 
-# cannot be changed.
-
-#you can also wrap up the sorted list as a tuple. that works too.
-#default dict, if key does not exist, make sone and sets. but I have not used
-# it here
-
-# How list(grouped_anagrams.values()) Works
-# This line transforms your dictionary of anagram groups into the final output format. Let me break it down:
-
-# grouped_anagrams.values()
-
-# Returns a special "view object" containing all values from your dictionary
-# In our case, these values are lists of strings (groups of anagrams)
-
-
-# list(...)
-
-# Converts this view object into a regular Python list
-# Creates a list of lists, where each inner list is one anagram group
-
-# grouped_anagrams = {
-#     "aet": ["eat", "tea", "ate"],
-#     "ant": ["tan", "nat"]
-# }
-
-# # grouped_anagrams.values() gives: dict_values([["eat", "tea", "ate"], 
-# # ["tan", "nat"]])
-# # list(...) converts to: [["eat", "tea", "ate"], ["tan", "nat"]]
+        return list(groups.values())
+            
         
