f74902c48fcbf92c194b29d6e932a105eef674e3
aleaiacta3st <invicinc@gmail.com>
2026-01-23 07:38:59 -0500

    Time: 15 ms (42.72%), Space: 19.5 MB (13.87%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,29 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        n=len(s)
-        m=len(t)
-        if n!=m:
+        if len(s)!=len(t):
             return False
-        t_scan = {}
-        for i in range(m):
-            if t[i] not in t_scan:
-                t_scan[t[i]]=1
-            else:
-                t_scan[t[i]]+=1
-        distinct_chars = len(t_scan)
-        left=0
-        for i in range(n):
-            if s[i] in t_scan:
-                t_scan[s[i]]-=1
-                if t_scan[s[i]]==0:
-                    distinct_chars-=1
-        return (distinct_chars==0)
 
-#easy when you have solved minimum window problem before
-# pitfall: in min window you check if t exists in s 
-# so it is possible that length s > length t 
-# but in this problem, lengths need to be equal 
+        char_freq={}
 
-# after the initial check, we just see if all characters of t
-# are found in s with the same frequency.
+        for c in s:
+            char_freq[c]=char_freq.get(c,0)+1
+
+
+        for c in t:
+            char_freq[c]=char_freq.get(c,0)-1
+            if char_freq[c]<0:
+                return False
+
+
+        return True
+
         
