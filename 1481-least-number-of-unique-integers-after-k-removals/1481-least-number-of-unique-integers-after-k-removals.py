83dbbd10b1a78de717fe03c6d248373df04bc325
aleaiacta3st <invicinc@gmail.com>
2026-02-05 01:14:23 -0500

    Time: 322 ms (75.41%), Space: 36.3 MB (42.27%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
@@ -3,21 +3,20 @@ class Solution:
 
         freq={}
 
-        n=len(arr)
 
         for num in arr:
-            freq[num] = freq.get(num,0)+1
+            freq[num] = freq.get(num,0)+1 
 
 
-        sorted_freqs =sorted(freq.values())
+
+        sorted_freq = sorted(freq.values())
 
         removed =0
 
-
-        for f in sorted_freqs:
+        for f in sorted_freq:
             if k>=f:
                 k=k-f
-                removed=removed+1
+                removed=removed+1 
             else:
                 break 
 
@@ -25,7 +24,6 @@ class Solution:
         return len(freq)-removed
 
 
-        
 
 
         
