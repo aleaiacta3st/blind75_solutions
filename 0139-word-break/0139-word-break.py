9142607419a28e80bd936ded5e8cf9e642c5b3f8
aleaiacta3st <invicinc@gmail.com>
2026-02-04 23:37:30 -0500

    Time: 3 ms (69.77%), Space: 19.5 MB (15.34%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -3,13 +3,15 @@ class Solution:
 
         n=len(s)
 
+        word_set=set(wordDict)
+
         dp=[False]*(n+1)
 
         dp[0] = True 
 
         for i in range(1,n+1):
             for j in range(0,i):
-                if dp[j]==True and s[j:i] in wordDict:
+                if dp[j]==True and s[j:i] in word_set:
                     dp[i]=True 
 
 
