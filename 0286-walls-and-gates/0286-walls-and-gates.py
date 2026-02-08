fa5226176891af46d1acb0b857313121043211f8
aleaiacta3st <invicinc@gmail.com>
2026-02-08 02:48:21 -0500

    Time: 27 ms (93.54%), Space: 22 MB (67.74%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -3,29 +3,21 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        treasures=set()
-
-        m=len(rooms)
-        n=len(rooms[0])
-
-        for r in range(m):
-                for c in range(n):
-                    if rooms[r][c]==0:
-                        treasures.add((r,c))
-
-        def bfs(treasures):
-            queue=deque(treasures)
-            visited=set(treasures)
-            while queue:
-                r,c=queue.popleft()
-                for dr,dc in [(0,1),(0,-1),(1,0),(-1,0)]:
-                    nr,nc=r+dr,c+dc
-                    if 0<=nr<m and 0<=nc<n and (nr,nc) not in visited and rooms[nr][nc] == 2147483647:
-                        rooms[nr][nc]=rooms[r][c]+1
-                        visited.add((nr,nc))
-                        queue.append((nr,nc))
-
-
-
-
-        bfs(treasures)
+        if not rooms:
+            return
+        m, n = len(rooms), len(rooms[0])
+        INF = 2147483647
+        q = deque()
+        
+        for i in range(m):
+            for j in range(n):
+                if rooms[i][j] == 0:
+                    q.append((i, j))
+        
+        while q:
+            r, c = q.popleft()
+            for dr, dc in [(1,0),(-1,0),(0,1),(0,-1)]:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < m and 0 <= nc < n and rooms[nr][nc] == INF:
+                    rooms[nr][nc] = rooms[r][c] + 1
+                    q.append((nr, nc))
